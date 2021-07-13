386e13c31cb2985894c58b845589b878bb48b3b9
gahvs <ga.vs_@outlook.com>
2021-07-13 10:41:35 -0300

    make firebird gbak backup

---
+++ bkp.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\Firebird\Firebird_2_5\bin
+gbak -user SYSDBA -password masterkey "C:\M3Soft\Banco de Dados\DADOS.FDB" "C:\M3Soft\Banco de Dados\BKP.FBK"
--- file/File.py
+++ file/File.py
@@ -1,11 +1,23 @@
+import os
+
 class File:
 
     def __init__(self) -> None:
         self.__file = None
+        self.__bkpFileName = "bkp.bat"
+        self.__BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    def __getFBBinPath(self) -> str or None:
+        try:
+            conf = open("conf", 'r')
+            return [_ for _ in conf][0]
+        except FileNotFoundError:
+            return None
+
 
     def loadFile(self) -> bool:
         try:
-            self.__file = open("conexao.txt", 'r')
+            self.__file = open("conn", 'r')
             return True
         except FileNotFoundError:
             return False
@@ -16,3 +28,29 @@ class File:
                 if key_search in line:
                     return line.split(key_search)[-1][:-1]
         return None
+    
+    def __makeBackupBatFile(self) -> bool:
+        #need the file to be loaded
+        dbPath = self.getDBPath(key_search='M3=')
+        pathCommand = "cd %s\n" % self.__getFBBinPath()
+        bkpCommand = 'gbak -user SYSDBA -password masterkey "%s" "%s"' % (dbPath, dbPath.replace("DADOS.FDB", "BKP.FBK"))
+        
+        try:
+            batFile = open(self.__bkpFileName, "w")
+            batFile.writelines([pathCommand, bkpCommand])
+            batFile.close()
+            return True
+        except:
+            return False
+
+    def makeBackup(self) -> bool:
+        if self.__makeBackupBatFile():
+            try:
+                import subprocess
+            except ImportError:
+                return False
+            
+            subprocess.call(os.path.join(self.__BASE_DIR, self.__bkpFileName))
+            return True
+        
+        return False
